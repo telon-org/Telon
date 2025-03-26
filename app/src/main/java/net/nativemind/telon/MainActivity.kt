754b7f8d1c46d8911adc3f3f845c4924309a55cf
Phakin Kongkha <secured.ntrain@gmail.com>
2025-03-26 13:42:07 +0700

    Add playback button, encapsulate AudioRecord into Recording

--- app/src/main/java/net/nativemind/telon/MainActivity.kt
+++ app/src/main/java/net/nativemind/telon/MainActivity.kt
@@ -2,16 +2,22 @@ package net.nativemind.telon
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.AudioAttributes
 import android.media.AudioFormat
-import android.media.AudioRecord
+import android.media.AudioTrack
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
+import kotlin.io.path.Path
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recStatus: TextView
+    private lateinit var recording: Recording
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,23 +27,55 @@ class MainActivity : AppCompatActivity() {
         recStatus = findViewById(R.id.recStatus)
 
         // Initialize audio recorder
-        val audioFormat: AudioFormat = Recording.getBestAudioFormat()
-        val audioRecord: AudioRecord? =
-            if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
-                Recording.initialize(audioFormat)
-            } else null
-        Recording.fetchRecordingStatus(this, recStatus, audioRecord)
-
-        // Bind button to onClickListener
-        // Recording
-        findViewById<Button>(R.id.btnRecStart).setOnClickListener {
-            audioRecord?.startRecording()
-            // TODO: Record audio to file
-            Recording.fetchRecordingStatus(this, recStatus, audioRecord)
-        }
-        findViewById<Button>(R.id.btnRecStop).setOnClickListener {
-            audioRecord?.stop()
-            Recording.fetchRecordingStatus(this, recStatus, audioRecord)
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            recording = Recording(Recording.getBestAudioFormat())
+            recording.setStatusView(this, recStatus)
+            // Bind button to onClickListener
+            findViewById<Button>(R.id.btnRecStart).setOnClickListener {
+                recording.recordToFile(Path(filesDir.toString(), "temp.pcm"))
+            }
+            findViewById<Button>(R.id.btnRecStop).setOnClickListener {
+                recording.stopRecord()
+            }
+            findViewById<Button>(R.id.btnRecPlayback).setOnClickListener {
+                Log.d(packageName, fileList()[0].toString())
+                val audioFormat = AudioFormat.Builder()
+                    .setSampleRate(48000)
+                    .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                    .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                    .build()
+                val bufferSizeInBytes = 2 * AudioTrack.getMinBufferSize(
+                    audioFormat.sampleRate,
+                    audioFormat.channelMask,
+                    audioFormat.encoding
+                )
+                val audioTrack: AudioTrack = AudioTrack.Builder()
+                    .setAudioAttributes(
+                        AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                            .build()
+                    )
+                    .setAudioFormat(audioFormat)
+                    .setBufferSizeInBytes(bufferSizeInBytes)
+                    .build()
+                val audioByteBuffer: ByteBuffer =
+                    ByteBuffer.allocateDirect(bufferSizeInBytes)
+                val fileChannel: FileChannel = openFileInput("temp.pcm").channel
+                var bytesRead: Int
+                while (true) {
+                    audioByteBuffer.clear()
+                    bytesRead = fileChannel.read(audioByteBuffer)
+                    Log.v(packageName, "Bytes read: $bytesRead")
+                    if (bytesRead <= 0) {
+                        break
+                    }
+                    audioByteBuffer.flip()
+                    audioTrack.write(audioByteBuffer, bytesRead, AudioTrack.WRITE_BLOCKING)
+                }
+                fileChannel.close()
+                audioTrack.play()
+            }
         }
         // TODO: Call
     }
--- app/src/main/java/net/nativemind/telon/Recording.kt
+++ app/src/main/java/net/nativemind/telon/Recording.kt
@@ -1,63 +1,111 @@
 package net.nativemind.telon
 
-import android.content.Context
+import android.app.Activity
 import android.graphics.Color
 import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaRecorder
+import android.util.Log
 import android.widget.TextView
 import androidx.annotation.RequiresPermission
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.util.EnumSet
 
-class Recording {
+class Recording// Build and return audio record
+@RequiresPermission(value = "android.permission.RECORD_AUDIO")
+constructor(audioFormat: AudioFormat) {
+    private var audioRecord: AudioRecord
+    private lateinit var statusView: TextView
+    private lateinit var activity: Activity
+    private var bufferSizeInBytes: Int = 0
+    private var continueRecording: Boolean = false
+
+    init {
+        bufferSizeInBytes = 2 * AudioRecord.getMinBufferSize(
+            audioFormat.sampleRate,
+            audioFormat.channelMask,
+            audioFormat.encoding
+        )
+        Log.d(Recording::class.java.simpleName, "Audio buffer size: $bufferSizeInBytes")
+        audioRecord = AudioRecord.Builder()
+            .setAudioSource(MediaRecorder.AudioSource.DEFAULT)
+            .setAudioFormat(audioFormat)
+            .setBufferSizeInBytes(bufferSizeInBytes)
+            .build()
+    }
+
+    fun setStatusView(activity: Activity, statusView: TextView) {
+        this.statusView = statusView
+        this.activity = activity
+        fetchRecordingStatus()
+    }
+
+    fun recordToFile(filePath: Path) {
+        if (audioRecord.recordingState != AudioRecord.RECORDSTATE_RECORDING) {
+            val audioByteBuffer = ByteBuffer.allocateDirect(bufferSizeInBytes)
+            val fileOutputChannel = FileChannel.open(
+                filePath, EnumSet.of(
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING,
+                    StandardOpenOption.WRITE
+                )
+            )
+            audioRecord.startRecording()
+            continueRecording = true
+            fetchRecordingStatus()
+            Thread {
+                while (continueRecording) {
+                    audioByteBuffer.clear()
+                    audioRecord.read(audioByteBuffer, bufferSizeInBytes, AudioRecord.READ_BLOCKING)
+                    Log.v(javaClass.simpleName, "Got message: $audioByteBuffer")
+                    fileOutputChannel.write(audioByteBuffer)
+                }
+
+                audioRecord.stop()
+                fileOutputChannel.close()
+
+                activity.runOnUiThread {
+                    fetchRecordingStatus()
+                }
+                Log.d(javaClass.simpleName, "Closing")
+            }.start()
+        }
+    }
+
+    fun stopRecord() {
+        continueRecording = false
+    }
+
+    private fun fetchRecordingStatus() {
+        if (audioRecord.state == AudioRecord.STATE_UNINITIALIZED) {
+            statusView.text = this.activity.resources.getString(R.string.uninitalized)
+            statusView.setTextColor(Color.RED)
+            return
+        }
+        when (audioRecord.recordingState) {
+            AudioRecord.RECORDSTATE_RECORDING -> {
+                statusView.text = this.activity.resources.getString(R.string.active)
+                statusView.setTextColor(Color.RED)
+            }
+
+            AudioRecord.RECORDSTATE_STOPPED -> {
+                statusView.text = this.activity.resources.getString(R.string.inactive)
+                statusView.setTextColor(Color.BLACK)
+            }
+        }
+    }
+
     companion object {
         fun getBestAudioFormat(): AudioFormat {
             // TODO: Check for compatible sample rate
             return AudioFormat.Builder()
                 .setSampleRate(48000)
                 .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                .setChannelMask(AudioFormat.CHANNEL_IN_STEREO)
+                .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
                 .build()
-        }
-
-        @RequiresPermission(value = "android.permission.RECORD_AUDIO")
-        fun initialize(audioFormat: AudioFormat): AudioRecord {
-            // Build and return audio record
-            val minBufSize = AudioRecord.getMinBufferSize(
-                audioFormat.sampleRate,
-                audioFormat.channelMask,
-                audioFormat.encoding
-            )
-            val audioRecord =
-                AudioRecord.Builder()
-                    .setAudioSource(MediaRecorder.AudioSource.MIC)
-                    .setAudioFormat(audioFormat)
-                    .setBufferSizeInBytes(2 * minBufSize)
-                    .build()
-            return audioRecord
-        }
-        // TODO: Record audio to file method
-
-        fun fetchRecordingStatus(
-            context: Context,
-            textStatus: TextView,
-            audioRecord: AudioRecord?
-        ) {
-            if (audioRecord == null || audioRecord.state == AudioRecord.STATE_UNINITIALIZED) {
-                textStatus.text = context.resources.getString(R.string.uninitalized)
-                textStatus.setTextColor(Color.RED)
-                return
-            }
-            when (audioRecord.recordingState) {
-                AudioRecord.RECORDSTATE_RECORDING -> {
-                    textStatus.text = context.resources.getString(R.string.active)
-                    textStatus.setTextColor(Color.RED)
-                }
-
-                AudioRecord.RECORDSTATE_STOPPED -> {
-                    textStatus.text = context.resources.getString(R.string.inactive)
-                    textStatus.setTextColor(Color.BLACK)
-                }
-            }
         }
     }
 }
