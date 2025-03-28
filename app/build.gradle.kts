af9d9f34827f786fbe2db9f9bb36e7d149f6267b
Phakin Kongkha <secured.ntrain@gmail.com>
2025-03-29 02:34:37 +0700

    Change version name to 0.0.1a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1a"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
